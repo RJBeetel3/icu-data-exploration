864dbe39e8a13751f1b93c2e1c3d71480c4184c7
Rob Beetel <robert@healthpalsinc.com>
2021-01-01 23:22:13 -0800

    refactors date-time type casting

--- icu_mortality_prediction/icu_mortality_prediction/src/features/ptnt_demog.py
+++ icu_mortality_prediction/icu_mortality_prediction/src/features/ptnt_demog.py
@@ -184,7 +184,6 @@ def create_diagnoses_df(ptnt_demog_df, diagnoses_bm_list, diagnoses):
 
     Returns
     -------
-    ptnt_demog_df: pandas DataFrame
     diagnoses2: pandas DataFrame
                     diagnoses index by icustays
 
@@ -205,27 +204,12 @@ def create_diagnoses_df(ptnt_demog_df, diagnoses_bm_list, diagnoses):
     for row in diagnoses.iterrows():
         if row[1]['USE_IN_BENCHMARK'] == 1:
             diagnoses2.loc[row[0]][row[1]['HCUP_CCS_2015']] = 1
-    ptnt_demog_df = ptnt_demog_df[cols]
-    return ptnt_demog_df, diagnoses2
+
+    return diagnoses2
     
 
         
-def continuous_to_categorical(ptnt_demog_data2_df, columns_by_data_type_dict):
-    demog_stats = ptnt_demog_data2_df[columns_by_data_type_dict['float64_cols']].dropna().describe()
-    print(demog_stats)
-    for col in ptnt_demog_data2_df[columns_by_data_type_dict['float64_cols']]:
-            Q1 = demog_stats[col].loc['25%']
-            Q2 = demog_stats[col].loc['50%']
-            Q3 = demog_stats[col].loc['75%']
-            ptnt_demog_data2_df[col] = ptnt_demog_data2_df[col].apply(lambda x: quant_cats(x, Q1, Q2, Q3))
-    
-    return ptnt_demog_data2_df
 
-def categorical_to_dummies(ptnt_demog_data2_df):
-    ptnt_demog_data2_df = pd.get_dummies(ptnt_demog_data2_df, columns=ptnt_demog_data2_df.columns[1:])
-
-    return ptnt_demog_data2_df
-    
 
 
 def write_best_features(dummies):
@@ -261,37 +245,6 @@ def write_best_features(dummies):
     features_df[features_df.p_values < .001].to_csv(root + name2)
     y = pd.DataFrame(y)
     y.to_csv(root + 'outcomes.csv')
-
-
-def group_column_names_by_data_type(data_df):
-    """
-     Groups columns by data type and returns each type in a list
-
-
-        Parameters
-        ----------
-        jdata_df: set
-                    set of unique id's
-
-        Returns
-        -------
-        columns_by_data_type_dict: dictionary
-                    dictionary with data type as key and list of corresponding column names as values
-                    dictionary with data type as key and list of corresponding column names as values
-    """
-    columns_by_data_type_dict = {}
-    # collect feature column names by type
-    columns_by_data_type_dict["float64_cols"] = [index for index, val in
-                                                 data_df.dtypes.iteritems() if val == 'float64']
-    # uint_cols = [index for index, val in labels_and_features_dropped_null_cols_df.dtypes.iteritems() if val=='uint8']
-    columns_by_data_type_dict["categorical_cols"] = [index for index, val in
-                                                     data_df.dtypes.iteritems() if val == 'object']
-    columns_by_data_type_dict["bool_cols"] = [index for index, val in
-                                              data_df.dtypes.iteritems() if val == 'bool']
-    columns_by_data_type_dict["int64_cols"] = [index for index, val in
-                                                data_df.dtypes.iteritems() if val == 'int64']
-
-    return columns_by_data_type_dict
 
 
 
--- icu_mortality_prediction/icu_mortality_prediction/src/utils/data_utils.py
+++ icu_mortality_prediction/icu_mortality_prediction/src/utils/data_utils.py
@@ -1,12 +1,53 @@
-def group_column_names_by_data_type(self):
-    self.columns_by_data_type_dict = {}
+import os
+import pandas as pd
+import numpy as np
+
+
+def group_column_names_by_data_type(data_df):
+    """
+     Groups columns by data type and returns each type in a list
+
+
+        Parameters
+        ----------
+        jdata_df: set
+                    set of unique id's
+
+        Returns
+        -------
+        columns_by_data_type_dict: dictionary
+                    dictionary with data type as key and list of corresponding column names as values
+                    dictionary with data type as key and list of corresponding column names as values
+    """
+    columns_by_data_type_dict = {}
     # collect feature column names by type
-    self.columns_by_data_type_dict["float64_cols"] = [index for index, val in
-                                                      self.data.dtypes.iteritems() if val == 'float64']
+    columns_by_data_type_dict["float64_cols"] = [index for index, val in
+                                                 data_df.dtypes.iteritems() if val == 'float64']
     # uint_cols = [index for index, val in labels_and_features_dropped_null_cols_df.dtypes.iteritems() if val=='uint8']
-    self.columns_by_data_type_dict["categorical_cols"] = [index for index, val in
-                                                          self.data.dtypes.iteritems() if val == 'object']
-    self.columns_by_data_type_dict["bool_cols"] = [index for index, val in
-                                                   self.data.dtypes.iteritems() if val == 'bool']
-    self.columns_by_data_type_dict["int64_cols"] = [index for index, val in
-                                                    self.data.dtypes.iteritems() if val == 'int64']
+    columns_by_data_type_dict["categorical_cols"] = [index for index, val in
+                                                     data_df.dtypes.iteritems() if val == 'object']
+    columns_by_data_type_dict["bool_cols"] = [index for index, val in
+                                              data_df.dtypes.iteritems() if val == 'bool']
+    columns_by_data_type_dict["int64_cols"] = [index for index, val in
+                                                data_df.dtypes.iteritems() if val == 'int64']
+    columns_by_data_type_dict["datetime64[ns]"] = [index for index, val in
+                                                data_df.dtypes.iteritems() if val == 'datetime64[ns]']
+
+    return columns_by_data_type_dict
+
+def continuous_to_categorical(ptnt_demog_data2_df, columns_by_data_type_dict):
+    demog_stats = ptnt_demog_data2_df[columns_by_data_type_dict['float64_cols']].dropna().describe()
+    print(demog_stats)
+    for col in ptnt_demog_data2_df[columns_by_data_type_dict['float64_cols']]:
+        Q1 = demog_stats[col].loc['25%']
+        Q2 = demog_stats[col].loc['50%']
+        Q3 = demog_stats[col].loc['75%']
+        ptnt_demog_data2_df[col] = ptnt_demog_data2_df[col].apply(lambda x: quant_cats(x, Q1, Q2, Q3))
+
+    return ptnt_demog_data2_df
+
+
+def categorical_to_dummies(ptnt_demog_data2_df):
+    ptnt_demog_data2_df = pd.get_dummies(ptnt_demog_data2_df, columns=ptnt_demog_data2_df.columns[1:])
+
+    return ptnt_demog_data2_df
